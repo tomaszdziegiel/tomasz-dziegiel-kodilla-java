ba949380da9eea0cc32adb8f5236b668e4a47da0
tomaszdziegiel <tomasz@dziegiel.org>
2018-04-18 17:50:09 +0200

    First Commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+public class Calculator {
+
+    public void somemetod() {
+
+    }
+
+    public static void main(String args[]){
+        Calculator calculator = new Calculator();
+        calculator.somemetod();
+    }
+}
