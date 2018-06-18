752c3056b0e51c6ce2ad2b6a7ee482534b049bd1
tomaszdziegiel <tomasz@dziegiel.org>
2018-06-18 18:13:47 +0200

    calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,9 +1,42 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
 
     public static void main(String[] args){
 
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("Test OK");
+} else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+
+        double resultc = calculator.add(2,3);
+        double resultd = calculator.subtract(3,2);
+
+        if (resultc == 5){
+            System.out.println("Test OK");
+        }else{
+            System.out.println("Error!");
+        }
+
+        if (resultd == 1){
+            System.out.println("Test OK");
+        }else{
+            System.out.println("Error!");
+        }
+
+
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+}
+
